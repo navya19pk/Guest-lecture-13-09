24c309327af68b1e85fdd016029d0a0c4b1570d2
navya <navyapkhajuria@gmail.com>
2025-09-13 13:52:27 +0530

    Create CODE_2.cpp

---
+++ CODE_2.cpp
@@ -0,0 +1,25 @@
+//Navya Pawan Khajuria 
+//24070123151
+//B3
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    int a = s.top(); s.pop();
+    int b = s.top(); s.pop();
+    s.push(a + b); 
+    s.push(3);
+    a = s.top(); s.pop();
+    b = s.top(); s.pop();
+    s.push(b * a); 
+    cout << "Result: " << s.top() << endl; // Output: 9
+    return 0;
+}
+
+//Output:
+//Result: 9
